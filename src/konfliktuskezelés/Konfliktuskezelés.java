0bdb32ea97bd712e619688d3fc417db5f9c46a12
Jegesmaci583 <80511513+Jegesmaci583@users.noreply.github.com>
2022-03-10 12:28:28 +0100

    2.név inicializálás

--- src/konfliktuskezelés/Konfliktuskezelés.java
+++ src/konfliktuskezelés/Konfliktuskezelés.java
@@ -2,7 +2,7 @@ package konfliktuskezelés;
 public class Konfliktuskezelés {
     public static void main(String[] args) {
         String nev1;
-        String nev2;
+        String nev2xx;
     }
     
 }
